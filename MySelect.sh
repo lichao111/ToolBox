0f7285c7ccc031225942381ed2558b607e09a403
Li Chao <515359166@qq.com>
2021-01-30 11:04:10 +0800

    逗号分隔字符转为数组，遍历数组，字符截取，进制转换

---
+++ MySelect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TerminalList=$1
+TerminalArry=(${TerminalList//,/ })
+for var in ${TerminalArry[@]}
+do
+  a1=$(echo $var|cut -c1-4)
+  a2=$(echo $var|cur -c5-8)
+  A2=$((0x$a2))
+  Addr="S\$10002\$${a1}\$${A2}\$\$"
+  echo $Addr $(redis-cli -p 1234 -a xxxx -h 127.0.0.1 -c hget $Addr TADDR 2>&1 |grep -v Warnin|grep -v nil)
+done
